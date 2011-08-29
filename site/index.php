0152eb99e49741d1a81371cb5f0259ae3a277986
joshreisner <josh@joshreisner.com>
2011-08-29 21:17:44 +0000

    fixing database issue with languages

--- site/index.php
+++ site/index.php
@@ -8,11 +8,19 @@ if ($posting) {
 	db_save('app', 1, 'post', false);
 
 	//process changes to languages
-	$languages_checked = array_checkboxes('languages');
+	$codes		= array_key_promote(db_table('SELECT id, code FROM app_languages'));
+	$languages_checked	= array_checkboxes('languages');
 	db_query('UPDATE app_languages SET checked = 0');
-	foreach ($languages_checked as $l) db_query('UPDATE app_languages SET checked = 1 WHERE id = ' . $l['id']);
+	foreach ($languages_checked as $l) {
+		db_query('UPDATE app_languages SET checked = 1 WHERE id = ' . $l['id']);
+
+		//check to make sure that fields are translated
+		$fields = db_table('SELECT f.type, f.field_name, o.table_name FROM app_fields f JOIN app_objects o ON f.object_id = o.id WHERE f.is_translated = 1 and f.is_active = 1');
+		foreach ($fields as $f) if (!db_column_exists($f['table_name'], $f['field_name'] . '_' . $codes[$l])) db_column_add($f['table_name'], $f['field_name'] . '_' . $codes[$l], $f['type']);
+	}
 	url_change_post('../');
 }
+
 echo drawFirst('Site Settings');
 
 $t = new form('app', 1, 'Edit Site Settings');
