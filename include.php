a4207c37ecd2b1173f2caeeebe92486e4854a6c7
joshreisner <josh@joshreisner.com>
2011-09-20 22:18:58 +0000

    stripping tags from text in list

--- include.php
+++ include.php
@@ -355,7 +355,7 @@ function drawObjectList($object_id, $from_type=false, $from_id=false, $from_ajax
 					$r[$f['field_name']] = format_string(strip_tags($r[$f['field_name']]), 50);
 				} elseif (($f['type'] == 'text') || ($f['type'] == 'email')) {
 					//$r[$f['field_name']] = 'text';
-					//$r[$f['field_name']] = format_string($r[$f['field_name']], 50);
+					$r[$f['field_name']] = format_string(strip_tags($r[$f['field_name']]), 50);
 				} else {
 					$r[$f['field_name']] = 'unhandled type';
 				}
