b5ecbf14e28fd799ce5dc97e7929018b36898f62
joshreisner <josh@joshreisner.com>
2011-10-14 19:30:56 +0000

    updating tinymce update

--- ajax/tinymce_update.php
+++ ajax/tinymce_update.php
@@ -5,7 +5,7 @@ $array = array_ajax();
 $rows = 0;
 $fields = db_table('SELECT f.field_name, o.table_name FROM app_fields f JOIN app_objects o ON f.object_id = o.id WHERE f.is_active = 1 AND o.is_active = 1 AND f.type = "textarea"');
 foreach ($fields as $f) {
-	db_query('UPDATE ' . $f['table_name'] . ' SET ' . $f['field_name'] . ' = REPLACE(' . $f['field_name'] . ', "' . $array['old_server'] . '/", "' . $request['host'] . '/")');
-	$rows .= mysql_affected_rows();
+	db_query('UPDATE ' . $f['table_name'] . ' SET ' . $f['field_name'] . ' = REPLACE(' . $f['field_name'] . ', "' . $array['old_server'] . '/", "' . $request['sanswww'] . '/")');
+	$rows += mysql_affected_rows();
 }
 echo format_quantitize($rows, 'row', false) . ' affected';
